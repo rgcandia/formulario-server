db1669fcc521d0e157b2463094448ca938b5a82b
Ale Candia <alejandrogcandia@gmail.com>
2023-09-12 09:55:33 -0300

    se crea archivo socket.js con la configuracion basica de el servidor socket.

---
+++ src/socket.js
@@ -0,0 +1,33 @@
+const {Server} = requuire('socket.io');
+
+let io;
+
+
+// Función para inicializar el SOCKET con el httpServer pasado por parámetro.
+function initialSocket(httpServer) {
+    io = new Server(httpServer, {
+      cors: {
+        origin: '*',
+      }
+    });
+  
+    // Pongo a escuchar eventos de conexión
+    io.on('connection', (socket) => {
+      console.log(`Connected: ${socket.id}`);
+  
+      socket.on('disconnect', () => {
+        console.log(`Disconnected: ${socket.id}`);
+      });
+
+    });
+
+
+
+
+
+    
+
+    return io;
+  }
+  
+  module.exports = initialSocket;
